7b2df3c4eac4e94a9daa4510bff49771f3152745
jgmsilva <58925924+jgmsilva@users.noreply.github.com>
2022-12-29 20:23:21 -0300

    add nix flake support

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+      };
+  outputs = { self, nix-doom-emacs, emacs-overlay, flake-utils }:
+  let
+    system = "x86_64-linux";
+    doom-emacs = nix-doom-emacs.packages.${system}.default.override {
+      doomPrivateDir = ./.;
+    };
+    inherit(flake-utils.lib) mkApp;
+  in {
+
+    packages.x86_64-linux.doom-emacs = doom-emacs;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.doom-emacs;
+
+    apps.x86_64-linux.default = mkApp { drv = self.packages.x86_64-linux.default; exePath = "/bin/emacs"; };
+  };
+}

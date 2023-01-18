b76e3aff9347aaa1e2c85a914c441bc9d4f32275
jgmsilva <jgui.silva1510@gmail.com>
2023-01-18 17:30:39 -0300

    all inputs use same nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,17 @@
   description = "A very basic flake";
 
   inputs = {
-    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nix-doom-emacs = {
+      url = "github:nix-community/nix-doom-emacs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
-      };
+  };
   outputs = { self, nix-doom-emacs, emacs-overlay, flake-utils }:
   let
     system = "x86_64-linux";

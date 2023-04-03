6f25c1e89737baf500efe2b3eb3542f575a32fde
jgmsilva <jgui.silva1510@gmail.com>
2023-04-02 23:12:51 -0300

    create doom and default emacs

--- flake.nix
+++ flake.nix
@@ -18,11 +18,11 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; overlays = [ emacs-overlay.overlays.default ]; };
       doom-emacs = nix-doom-emacs.packages.${system}.default.override {
-        doomPrivateDir = ./.;
+        doomPrivateDir = ./doom;
         doomPackageDir = pkgs.linkFarm "my-doom-packages" [
           { name = "config.el"; path = pkgs.emptyFile; }
-          { name = "init.el"; path = ./init.el; }
-          { name = "packages.el"; path = ./packages.el; }
+          { name = "init.el"; path = ./doom/init.el; }
+          { name = "packages.el"; path = ./doom/packages.el; }
         ];
         extraPackages = epkgs: with pkgs;[
           fd
@@ -30,13 +30,24 @@
           ripgrep
         ];
       };
+      gnu-emacs = pkgs.emacs.pkgs.withPackages
+        (epkgs: (with epkgs.melpaStablePackages; [
+          (pkgs.runCommand "default.el" { } ''
+            mkdir -p $out/share/emacs/site-lisp
+            cp -r ${./gnu} $out/share/emacs/site-lisp/
+          '')
+          dracula-theme
+        ]));
       inherit (flake-utils.lib) mkApp;
     in
     {
-      inherit doom-emacs;
+      inherit doom-emacs gnu-emacs;
 
-      packages.x86_64-linux.default = self.doom-emacs;
+      packages.x86_64-linux.gnu-emacs = self.gnu-emacs;
+      packages.x86_64-linux.doom-emacs = self.doom-emacs;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.doom-emacs;
 
-      apps.x86_64-linux.default = mkApp { drv = self.packages.x86_64-linux.default; exePath = "/bin/emacs"; };
+      apps.x86_64-linux.gnu-emacs = mkApp { drv = self.packages.x86_64-linux.gnu-emacs; exePath = "/bin/emacs"; };
+      apps.x86_64-linux.doom-emacs = mkApp { drv = self.packages.x86_64-linux.doom-emacs; exePath = "/bin/emacs"; };
     };
 }
---
+++ gnu/default.el
@@ -0,0 +1,4 @@
+(load-theme 'dracula)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)

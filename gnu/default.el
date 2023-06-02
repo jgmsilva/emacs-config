2bb8ee1a9863bfd195945a03835a0e72a5105fca
jgmsilva <jgui.silva1510@gmail.com>
2023-06-02 16:09:03 -0300

    add transparency gnu

--- gnu/default.el
+++ gnu/default.el
@@ -1,4 +1,10 @@
-(load-theme 'dracula)
+(load-theme 'wombat)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(set-frame-parameter (selected-frame) 'alpha '(85 85))
+
+(add-to-list 'default-frame-alist '(alpha 85 85))
+
+(set-face-attribute 'default nil :background "black"
+  :foreground "white" :font "FiraCode NerdFont Mono" :height 180)
